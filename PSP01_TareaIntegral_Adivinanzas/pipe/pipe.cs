0308094d36ee90c390e18cca4a5ba341b8593b3e
sdalessio99 <119613479+sdalessio99@users.noreply.github.com>
2024-06-17 03:15:42 +0200

    Add files via upload

---
+++ PSP01_TareaIntegral_Adivinanzas/pipe/pipe.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+//enlace al video de youtube: https://youtu.be/q4zK7Rw_ldk 
+
+namespace pipe
+{
+    class Pipe
+    {
+        static void Main(string[] args)
+        {
+            //lanzar el proceso
+            Process p = null;
+            StartServer(out p);
+            Task.Delay(1000).Wait();
+
+            try
+            {
+                //conexion del cliente
+                var client = new NamedPipeClientStream("PSP01_TareaIntegral_NamedPipe");
+                client.Connect();
+                Console.WriteLine("Estableciendo conexión con el servidor");
+
+                //crear buffers
+                StreamReader sr = new StreamReader(client);
+                StreamWriter sw = new StreamWriter(client);
+
+                //llamada a metodo para procesar los datos
+                ProcesarDatos(sr, sw, p);
+
+                //matar el proceso
+                PararProceso(p);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Apagando servidor por un error: " + e.Message);
+            }
+        }
+
+        //Metodo StartServer para gestionar el proceso
+        static Process StartServer(out Process p1)
+        {
+            //declara variable interna ProcessStratInfo para la informacion del proceso a lanzar, ejemplo ruta del ejecutable y ajustes de ventana
+            ProcessStartInfo info = new ProcessStartInfo(@"..\..\..\..\pipeServidor\bin\Release\net8.0\publish\pipeServidor.exe");
+            info.CreateNoWindow = false;
+            info.WindowStyle = ProcessWindowStyle.Normal;
+            info.UseShellExecute = true;
+
+            //arrancar el proceso y guardar los datos de creacion en el Process p1 para matar o ver estado del proceso
+            p1 = Process.Start(info);
+
+            return p1;
+        }
+
+        //metodo para procesar los datos entre server y cliente
+        private static void ProcesarDatos(StreamReader sr, StreamWriter sw, Process pr)
+        {
+            //inicializa las variables que se usan para procesar los datos
+            string pregunta = string.Empty;
+            string respuesta = string.Empty;
+            bool seguirJugando = true;
+            bool acertado = false;
+            string servidor = string.Empty;
+
+            try
+            {
+                servidor = servidor + "J ";
+                sw.WriteLine(servidor);
+                sw.Flush();
+                Console.Write("*******************\n");
+                Console.Write("*Comienza el juego*\n");
+                Console.Write("*******************\n");
+
+                while (seguirJugando)
+                {
+                    while (!acertado)
+                    {
+                        //obtenemos lo que ha enviado el servidor, y nos quedamos con el primer caracter del buffer
+                        //las opciones son P (pregunta), R (respuesta) o F (fin) 
+                        //se va a usar el caracter para saber si el servidor esta mandando el enunciado de la adivinanza, el resultado de la respuesta
+                        //o si el programa ha finalizado
+                        servidor = sr.ReadLine();
+                        switch (servidor[0])
+                        {
+                            //en este caso, se obtiene el enunciado de la adivinanza quitandole los dos primeros caracteres al string ("P ")
+                            //se pasa el enunciado al metodo que imprime la cabecera y mandamos la primera respuesta al servidor leyendola de la consola
+                            //va a ir acompañada de R para que el servidor sepa que le estamos mandando 
+                            case 'P':
+                                pregunta = servidor.Substring(2, servidor.Length - 2);
+                                ImprimirCabecera(pregunta);
+                                respuesta = Console.ReadLine();
+                                respuesta = "R " + respuesta;
+                                sw.WriteLine(respuesta);
+                                sw.Flush();
+                                pregunta = string.Empty;
+                                respuesta = string.Empty;
+                                break;
+
+                            //en este caso, se obtiene lo que devuelve el servidor sin los caracteres iniciales
+                            //si contiene OK, se imprime el resultado en consola y se manda "J " al servidor para que sepa que queremos otra adivinanza
+                            //si no contiene OK, se imprime el resultado, se saca la respuesta nueva y se envia al servidor junto a R para que sepa que es una respuesta
+                            case 'R':
+                                pregunta = servidor.Substring(2, servidor.Length - 2);
+                                if (string.Equals(pregunta, "OK"))
+                                {
+                                    Console.Write("¡Enhorabuena! Has acertado.\n");
+                                    Console.Write("----------***--------------\n");
+                                    acertado = true;
+                                    servidor = string.Empty;
+                                    servidor = servidor + "J ";
+                                    sw.WriteLine(servidor);
+                                    sw.Flush();
+                                    Console.Write("*******************\n");
+                                    Console.Write("Juega otra vez:\n");
+                                    Console.Write("*******************\n");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.Write("No has acertado. Introduce otra respuesta:\n");
+                                    respuesta = Console.ReadLine();
+                                    respuesta = "R " + respuesta;
+                                    sw.WriteLine(respuesta);
+                                    sw.Flush();
+                                    break;
+                                }
+                            
+                            //en este caso se trata la opcion de F(fin) mandada por el servidor para cuando se acaban las adivinanzas del documento
+                            //si el proceso ya esta cerrado devuelve el id a consola, si no se mata y tambien devuelve el id
+                            case 'F':
+                                servidor = servidor + "F ";
+                                seguirJugando = false;
+                                acertado = true;
+                                Console.WriteLine("No quedan adivinanzas, por lo tanto cerramos el programa\n");
+
+                                if (pr == null)
+                                {
+                                    Console.Write(pr.Id.ToString());
+                                    Console.WriteLine("El proceso servidor ya no existe.");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.Write(pr.Id.ToString());
+                                    if ((sr != null) || (sw != null))
+                                    {
+                                        if (sr != null)
+                                        {
+                                            sr.Close();
+                                        }
+                                        else if (sw != null)
+                                        {
+                                            sw.Close();
+                                        }
+                                    }
+                                    pr.Kill();
+                                }
+                                break;
+
+                            default:
+                                Console.WriteLine("Opción {0} no válida.");
+                                seguirJugando = false;
+                                acertado = true;
+                                Console.WriteLine("Exit");
+                                servidor = null;
+                                break;
+                        }
+                    }
+                    acertado = false;
+                    servidor = servidor + "J ";
+                }
+            }
+            catch (Exception e)
+            {
+               Console.WriteLine("Error al procesar datos: " + e.Message);
+            }
+        }
+
+        //metodo para imprimir cabecera
+        private static void ImprimirCabecera(string enunciado)
+        {
+            Console.Write("\n");
+            Console.WriteLine("Resuelve el siguiente acertijo:\n");
+            Console.Write("******************Acertijo*************************\n");
+            Console.Write(enunciado + "\n");
+            Console.Write("****************************************************\n");
+            Console.Write("Indica una respuesta:\n");
+        }
+        
+        //metodo para matar el proceso
+        static void PararProceso(Process pr)
+        {
+            if (pr != null && !pr.HasExited)
+            {
+                pr.Kill();
+                pr = null;
+            }
+        }
+    }
+}
---
+++ PSP01_TareaIntegral_Adivinanzas/pipeServidor/pipeServidor.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Xml;
+
+//enlace al video de youtube: https://youtu.be/q4zK7Rw_ldk 
+
+namespace pipeServidor
+{
+    class PipeServidor
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //inicializar variables a usar en la logica del programa
+                string fichero = @"..\..\..\..\adivinanzas.xml";
+                List<Adivinanza> listaAdivinanzas = new List<Adivinanza>();
+                Adivinanza adivinanza1 = null;
+                string pregunta = string.Empty;
+                string respuesta = string.Empty;
+                string cliente = string.Empty;
+                bool seguirJugando = true;
+
+                //conexion del servidor
+                var server = new NamedPipeServerStream("PSP01_TareaIntegral_NamedPipe");
+                server.WaitForConnection();
+                Console.WriteLine("Conexión a servidor establecida.");
+                Console.WriteLine("Pipe Servidor esperando datos.\n");
+
+                //crear buffers
+                StreamReader sr = new StreamReader(server);
+                StreamWriter sw = new StreamWriter(server);
+
+                //cargar fichero en la lista
+                listaAdivinanzas = CargarFicheroXml(fichero);
+
+                while(seguirJugando)
+                {
+                    //obtenemos lo que ha enviado el cliente, y nos quedamos con el primer caracter del buffer
+                    //las opciones son J (jugar) o R (respuesta) 
+                    //se va a usar el caracter para saber si el usuario esta solicitando jugar o si está enviando una respuesta a una adivinanza
+                    cliente = sr.ReadLine();
+
+                    switch (cliente[0])
+                    {
+                        //este caso elige una adivinanza aleatoria para presentar al cliente
+                        //aprovecha ese metodo para hacer un control de cuantas quedan en la lista
+                        //si la adivinanza es null significa que se han acabado los acertijos y se cierra el programa, manda F (fin) al cliente
+                        //si la adivinanza tiene un valor significa que todavia quedan, manda P (pregunta) al cliente y el enunciado
+                        case 'J':
+                            adivinanza1 = ElegirAdivinanzaRandom(listaAdivinanzas);
+                            if(adivinanza1 != null)
+                            {
+                                pregunta = adivinanza1.Enunciado;
+                                pregunta = "P " + pregunta;
+                                sw.WriteLine(pregunta);
+                                sw.Flush();
+                                Console.WriteLine("Enviado:" + pregunta);
+                                break;
+                            }
+                            else
+                            {
+                                seguirJugando = false;
+                                pregunta = "F ";
+                                sw.WriteLine(pregunta);
+                                sw.Flush();
+                                Console.WriteLine("Finalizando programa, se han acabado las adivinanzas");
+                                break;
+                            }
+
+                        //este caso obtiene la respuesta del usuario y la compara con la respuesta acertada
+                        //si acierta devuelve "R OK", si no devuelve "R KO"
+                        case 'R':
+                            respuesta = cliente.Substring(2, cliente.Length - 2);
+                            Console.WriteLine("Recibido:\n" + respuesta);
+                            Console.WriteLine("RespuestaReal:" + adivinanza1.Respuesta);
+
+                            if (String.Equals(respuesta.ToLower(), adivinanza1.Respuesta.ToLower()))
+                            {
+                                pregunta = "R " + "OK";
+                                sw.WriteLine(pregunta);
+                                sw.Flush();
+                                Console.WriteLine("Enviado:" + pregunta);
+                                Console.WriteLine("Borrando adivinanza de la lista:" + adivinanza1.Enunciado);
+                                listaAdivinanzas.Remove(adivinanza1);
+                                Console.WriteLine("Número de adivinanzas disponibles en la lista:{0}\n", listaAdivinanzas.Count);
+                                break;
+                            }
+                            else
+                            {
+                                pregunta = "R " + "KO";
+                                sw.WriteLine(pregunta);
+                                sw.Flush();
+                                Console.WriteLine("Enviado:" + pregunta);
+                                break;
+                            }
+
+                        //se usa el caso default para tratar otras opciones, se avisa y se cambia el valor de seguirJugando a false y eso cierra el programa
+                        default:
+                            Console.WriteLine("La opcion no es valida. Se cerrará el programa");
+                            seguirJugando = false;
+                            cliente = null;
+                            break;
+                    }
+                }
+                //cierre de buffers
+                sr.Close();
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Apagando servidor por un error: " + ex.Message);
+            }
+        }
+
+        //metodo para cargar un fichero xml a una lista de objetos Adivinanza
+        private static List<Adivinanza> CargarFicheroXml(string rutaFichero)
+        {
+            //inicializar lista que queremos devolver
+            List<Adivinanza> lista = new List<Adivinanza>();
+            
+            //crear objeto XmlDocument para guardar el contenido del xml del que tenemos la ruta
+            XmlDocument fichero = new XmlDocument();
+            fichero.Load(rutaFichero);
+
+            //obtener los nodos que nos interesan
+            XmlNodeList nodos = fichero.SelectNodes("/adivinanzas/adivinanza");
+
+            //recorrer la lista de nodos para obtener los datos, crear un obj Adivinanza por cada nodo y añadirlo a la lista
+            foreach (XmlNode nodo in nodos)
+            {
+                int numero = int.Parse(nodo.Attributes["numero"].Value);
+                string enunciado = nodo.SelectSingleNode("enunciado").InnerText;
+                string respuesta = nodo.SelectSingleNode("respuesta").InnerText;
+
+                Adivinanza adivinanza1 = new Adivinanza(numero, enunciado, respuesta);
+                lista.Add(adivinanza1);
+            }
+            return lista;
+        }
+
+        //metodo para elegir y devolver una adivinanza aleatoria de la lista y controlar cuantas quedan para resolver
+        //si ya no quedan devuelve un obj Adivinanza con valor null
+        private static Adivinanza ElegirAdivinanzaRandom(List<Adivinanza> lista)
+        {
+            //inicializar el obj Adivinanza que queremos devolver y el indice que vamos a usar
+            Adivinanza adivinanza1 = null;
+            int indice = 0;
+
+            //crear obj Random para generar un numero aleatorio
+            Random azar = new Random();
+
+            if (lista.Count != 0)
+            {
+                indice = azar.Next(lista.Count);
+                Console.WriteLine(indice);
+                adivinanza1 = lista[indice];
+            }
+            else
+            {
+                adivinanza1 = null;
+            }
+
+            return adivinanza1;
+        }
+    }
+
+    class Adivinanza
+    {
+        public int Numero { get; set; }
+        public string Enunciado { get; set; }
+        public string Respuesta { get; set; }
+
+        public Adivinanza(int numero, string enunciado, string respuesta)
+        {
+            Numero = numero;
+            Enunciado = enunciado;
+            Respuesta = respuesta;
+        }
+
+        public override string ToString()
+        {
+            return "Adivinanza " + Numero + ": " + Enunciado + " - " + Respuesta; 
+        }
+    }
+}
